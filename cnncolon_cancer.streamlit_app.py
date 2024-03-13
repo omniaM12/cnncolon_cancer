b0a1e31329a104bb26aafc4ebcbd5d7932a89b07
omniaM12 <147537836+omniaM12@users.noreply.github.com>
2024-03-13 14:18:20 +0200

    Update cnncolon_cancer.streamlit_app.py

--- cnncolon_cancer.streamlit_app.py
+++ cnncolon_cancer.streamlit_app.py
@@ -16,7 +16,6 @@ if file_name is not None:
     col1, col2 = st.columns(2)
 
     image = Image.open(file_name)
-    image=image/255
     col1.image(image, use_column_width=True)
     predictions = model.predict(image)
 
